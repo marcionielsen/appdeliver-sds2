b4bde9da689a762079bf8705282e74f90de98538
marcio.nielsen <marcio.nielsen@gmail.com>
2021-01-04 20:49:23 -0300

    Criação do Projeto.

---
+++ backend/src/main/java/br/com/nielsen/appdeliver/AppdeliverApplication.java
@@ -0,0 +1,13 @@
+package br.com.nielsen.appdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/nielsen/appdeliver/AppdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nielsen.appdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
